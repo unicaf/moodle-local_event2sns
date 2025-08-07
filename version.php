c526ec170da1d1e43e7ffe49971e76b0f97fc0ac
Petros Andreou <petros.andreou88@gmail.com>
2025-08-07 12:14:30 +0300

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023022313;            // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version = 2023022315;            // The current plugin version (Date: YYYYMMDDXX)
 $plugin->release = '0.1';
 $plugin->requires = 2018051700;            // Requires this Moodle version.
 $plugin->component = 'local_event2sns';        // Full name of the plugin (used for diagnostics).
